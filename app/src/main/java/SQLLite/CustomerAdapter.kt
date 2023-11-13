7bc183cb50a5a4b5e3dc5dc600ecb29c2c5c9015
Joan148 <i202112603@cibertec.edu.pe>
2023-11-12 22:08:07 -0500

    cambios y arreglo de errores Register

--- app/src/main/java/SQLLite/CustomerAdapter.kt
+++ app/src/main/java/SQLLite/CustomerAdapter.kt
@@ -14,6 +14,8 @@ class CustomerAdapter(private val customerList : List<CustomerViewModel>) : Recy
         val imageCustomer = itemView.findViewById<ImageView>(R.id.img_firefoot)
         val name = itemView.findViewById<TextView>(R.id.lbl_name)
         val user = itemView.findViewById<TextView>(R.id.lbl_user)
+        val email = itemView.findViewById<TextView>(R.id.lbl_email)
+        val phone = itemView.findViewById<TextView>(R.id.lbl_phone)
 
 
     }
@@ -28,6 +30,8 @@ class CustomerAdapter(private val customerList : List<CustomerViewModel>) : Recy
         holder.imageCustomer.setImageResource(itemViewModel.image)
         holder.name.text = itemViewModel.fullName
         holder.user.text = itemViewModel.user
+        holder.email.text = itemViewModel.email
+        holder.phone.text = itemViewModel.phone
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/pe/cibertec/firefootstore/RegisterActivity.kt
+++ app/src/main/java/pe/cibertec/firefootstore/RegisterActivity.kt
@@ -1,5 +1,6 @@
 package pe.cibertec.firefootstore
 
+import SQLLite.BDHelper
 import SQLLite.CustomerAdapter
 import SQLLite.CustomerViewModel
 import android.content.Intent
@@ -7,39 +8,90 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var customerRecycler: RecyclerView
     private lateinit var adapter: CustomerAdapter
     private val registroList = ArrayList<CustomerViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        customerRecycler = findViewById(R.id.recyclerViewClientes)
+        customerRecycler.layoutManager = LinearLayoutManager(this)
 
+        // Configurar el RecyclerView con el adaptador
+        adapter = CustomerAdapter(registroList)
+        customerRecycler.adapter = adapter
 
+        val btnGrabar: Button = findViewById(R.id.btnRegisterCustom)
 
-        val btnRegistrar: Button = findViewById(R.id.btnRegisterCustom)
-        btnRegistrar.setOnClickListener {
-            val txtFullNames = findViewById<EditText>(R.id.RegisterTxtNames)
-            val txtUser = findViewById<EditText>(R.id.RegisterTxtUser)
-            val txtPwd = findViewById<EditText>(R.id.RegisterTxtPwd)
-            val txtEmail = findViewById<EditText>(R.id.RegisterTxtEmail)
-            val txtPhone = findViewById<EditText>(R.id.RegisterTxtPhone)
+        btnGrabar.setOnClickListener {
+            val txtFullName: EditText = findViewById(R.id.RegisterTxtNames)
+            val txtUser: EditText = findViewById(R.id.RegisterTxtUser)
+            val txtEmail: EditText = findViewById(R.id.RegisterTxtEmail)
+            val txtPhone: EditText = findViewById(R.id.RegisterTxtPhone)
 
-            val names = txtFullNames.text.toString()
+            val fullName = txtFullName.text.toString()
             val user = txtUser.text.toString()
-            val pwd = txtPwd.text.toString()
             val email = txtEmail.text.toString()
             val phone = txtPhone.text.toString()
+            val db = BDHelper(this, null)
+            db.createRegister(fullName, user, email, phone)
+            Toast.makeText(this, "Registro guardado", Toast.LENGTH_SHORT).show()
+            txtFullName.text.clear()
+            txtUser.text.clear()
+            txtEmail.text.clear()
+            txtPhone.text.clear()
+            // Llama a cargarRegistrosDesdeBD() para actualizar la lista
+            cargarRegistrosDesdeBD()
         }
+        val btnLimpiar: Button = findViewById(R.id.btnLimpiar)
+        btnLimpiar.setOnClickListener {
+            val db = BDHelper(this, null)
 
-        val btnSeeCustomers: Button = findViewById(R.id.btnSeeCustomers)
-        btnSeeCustomers.setOnClickListener {
-            val intent = Intent(this, CustomersActivity::class.java)
-            startActivity(intent)
+            // Borra los registros de la base de datos
+            db.clearRegisters()
+
+            // Borra los registros de la lista
+            registroList.clear()
+
+            // Notificar al adaptador que se han actualizado los datos
+            adapter.notifyDataSetChanged()
         }
 
     }
+
+    private fun cargarRegistrosDesdeBD() {
+        registroList.clear() // Limpia la lista antes de cargar registros
+
+        val db = BDHelper(this, null)
+        val cursor = db.customerRegisters()
+
+        if (cursor != null && cursor.moveToFirst()) {
+            do {
+                val fullnameIndex = cursor.getColumnIndex("NAMES")
+                val userIndex = cursor.getColumnIndex("USER")
+                val emailIndex = cursor.getColumnIndex("EMAIL")
+                val phoneIndex = cursor.getColumnIndex("PHONE")
+
+                val fName = cursor.getString(fullnameIndex)
+                val usuario = cursor.getString(userIndex)
+                val email = cursor.getString(emailIndex)
+                val phone = cursor.getString(phoneIndex)
+
+                // Agregar los registros a la lista dinámica
+                registroList.add(CustomerViewModel(R.drawable.ic_launcher_foreground, fName, usuario, email, phone))
+
+            } while (cursor.moveToNext())
+        }
+        cursor?.close()
+        // Notificar al adaptador que se han actualizado los datos
+        adapter.notifyDataSetChanged()
+    }
+
 }
